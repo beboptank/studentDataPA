401bdc752964c81eef94b61e468aba17655f666b
beboptank <beboptank12@gmail.com>
2023-04-06 13:24:47 -0400

    student.h file guarded

--- studentDataPA/student.h
+++ studentDataPA/student.h
@@ -1,3 +1,6 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
 #pragma once
 #include <iostream>
 #include <string>
@@ -40,3 +43,4 @@ void SetDegreeProgram(DegreeProgram degreeProgram);
 
 void print();
 
+#endif
