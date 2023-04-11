a439bda2995f6c9f132bfac3aa534052dd782d43
beboptank <beboptank12@gmail.com>
2023-04-11 16:12:29 -0400

    Able to print all students using printAll() function in main.cpp

--- studentDataPA/roster.cpp
+++ studentDataPA/roster.cpp
@@ -5,8 +5,19 @@
 
 using namespace std;
 
-Roster::Roster() {
+const string studentData[] =
 
+{ "A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY",
+	"A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
+	"A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE",
+	"A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
+	"A5,Seth,Christmus,schr297@wgu.edu,31,40,23,32,SOFTWARE"
+};
+
+Roster::Roster() {
+	for (int i = 0; i < numStudents; i++) {
+		Roster::parseData(studentData[i]);
+	}
 }
 
 Roster::~Roster() {
