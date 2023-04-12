ed3178fd7d46cc16d709edf3318c3e821b7edf9b
beboptank <beboptank12@gmail.com>
2023-04-12 12:09:47 -0400

    added endl statements for display formatting

--- studentDataPA/main.cpp
+++ studentDataPA/main.cpp
@@ -8,31 +8,39 @@ int main() {
 	cout << "Lanugage used: C++" << endl;
 	cout << "WGU Student ID: 005522686" << endl;
 	cout << "Name: Seth Christmus" << endl;
+	cout << endl;
 
 	const int numStudents = 5;
 	DegreeProgram SOFTWARE = DegreeProgram::SOFTWARE;
-
 	Roster classRoster;
+	cout << endl;
 
 	cout << "Displaying all students: " << endl;
 	classRoster.printAll();
+	cout << endl;
 
 	cout << "Displaying invalid emails: " << endl;
 	classRoster.printInvalidEmails();
+	cout << endl;
 
 	cout << "Displaying each student's average days to complete each course: " << endl;
 	for (int i = 0; i < numStudents; i++) {
 		classRoster.printAverageDaysInCourse(classRoster.getStudents()[i]->getStudentID());
 	}
+	cout << endl;
 
 	cout << "Displaying students in the SOFTWARE degree program: " << endl;
 	classRoster.printByDegreeProgram(SOFTWARE);
+	cout << endl;
 
 	cout << "Removing student A3 from the roster: " << endl;
 	classRoster.remove("A3");
+	cout << endl;
 	cout << "Updated roster: " << endl;
 	classRoster.printAll();
+	cout << endl;
 	classRoster.remove("A3");
+	cout << endl;
 	
 	cout << "Program complete. Calling destructors: " << endl;
 
